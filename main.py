d14e5c1a5c4b5b25c141df8aaa8fd3a418b17e6a
Fernando Camblor <fernandocamblor@fernandos-mbp.lan>
2025-07-19 12:10:14 -0400

    added a bunch of new stuff so it works now. yay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from styles.colors import COLORS
+from src.Player.Player import *
 
 width  = 900
 height = 900
@@ -8,6 +9,8 @@ height = 900
 pygame.init()
 screen = pygame.display.set_mode(( width, height ))
 clock = pygame.time.Clock()
+
+player = Player().setConfig(screen, width, height)
 
 while True:
     for event in pygame.event.get():
@@ -17,7 +20,7 @@ while True:
 
     screen.fill(COLORS['maroon'])
     # game code
-
+    player.spawn(width / 2, height / 2).control()
 
     pygame.display.update()
     clock.tick(60)
--- src/Player/Player.py
+++ src/Player/Player.py
@@ -1,12 +1,49 @@
 import pygame
+from styles.colors import COLORS
 
-health     = 2
-jumpHeight = 5
-speed      = 3
-coin       = 0
-inv        = []
-locationX  = 0.0
-locationY  = 0.0
+class Player:
+    def __init__(self):
+        self.health     = 3
+        self.jumpHeight = 5
+        self.speed      = 5
+        self.coin       = 0
+        self.inv        = []
+        self.playerSize = 10
+        self.locationX  = 0.0
+        self.locationY  = 0.0
+        self.surface    = None
+        self.height     = None
+        self.width      = None
 
-def spawn(x, y):
-    pass
+
+    def setConfig(self, surface, width, height):
+        self.surface = surface
+        self.width   = width
+        self.height  = height
+        return self
+
+    def spawn(self, x, y):
+        pygame.draw.circle(self.surface, COLORS['white'], (x, y), self.playerSize, width=0)
+        self.locationX = x
+        self.locationY = y
+        return self
+
+    def jump(self):
+        ground = self.height - 352
+
+        if self.locationY == ground:
+            self.locationY = ground 
+
+        else:
+            self.locationY -= 0.5
+
+    def control(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:   locationX -= self.speed
+        if keys[pygame.K_RIGHT]:  locationX += self.speed
+        if keys[pygame.K_UP]:     locationY -= self.speed
+        if keys[pygame.K_DOWN]:   locationY += self.speed
+        if keys[pygame.K_SPACE]:  self.jump() # jump
+
+        return self
+    
